103eaff1e74cef50c3ffd36a837a92fb167174df
sankalpjumale <sankalpjumale@gmail.com>
2026-06-11 21:39:38 +0530

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Link from "next/link";
 
-export default function Home() {
+
+export default async function Home() {
+
+  const {userId} = await auth()
+  if(!userId) redirect('/dashboard')
+
   return (
-    <div>
-      <h2 className="bg-amber-200">Personal Expence Tracker</h2>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">Personal Expence Tracker</h1>
+      <div className="flex gap-3">
+        <Button asChild>
+          <Link href="/sign-in">Sign-In</Link>
+        </Button>
+
+        <Button asChild variant="outline">
+          <Link href="/sign-up">Sign-Up</Link>
+        </Button>
+      </div>
     </div>
   );
 }
