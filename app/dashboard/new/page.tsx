708d6d9ff3d13874099d4d9ccd87e3629d0b2272
sankalpjumale <sankalpjumale@gmail.com>
2026-06-17 16:01:18 +0530

    refactor: NewExpensePage component

--- app/dashboard/new/page.tsx
+++ app/dashboard/new/page.tsx
@@ -1,11 +1,22 @@
 import { ExpenseForm } from '@/components/expense-form/ExpenseForm'
 import { createExpense } from '../action'
+import { auth } from '@clerk/nextjs/server'
+import { prisma } from '@/lib/generated/prisma'
 
-function NewExpensePage() {
+async function NewExpensePage() {
+
+  const {userId} = await auth()
+  if(!userId) return null
+
+  const categories = await prisma.category.findMany({
+    where: {userId},
+    orderBy: {name: 'asc'}
+  })
+
   return (
     <div>
         <h1 className='text-2xl font-bold mb-6'>Add Expense</h1>
-        <ExpenseForm action={createExpense}/>
+        <ExpenseForm action={createExpense} categories={categories}/>
     </div>
   )
 }
